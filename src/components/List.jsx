3ad52023628615e1752682b6d8d6eb4c3b5a588c
Imran Aliyev <imran.aliyev@gmail.com>
2016-02-08 23:57:30 +0400

    react skeleton has been prepared'

---
+++ src/components/List.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingridients = [
+  {
+    "id": 1,
+    "text": "ham"
+  }, {
+    "id": 2,
+    "text": "cheese"
+  }, {
+    "id": 3,
+    "text": "potato"
+  }
+]
+
+var List = React.createClass({
+  render: function() {
+    var listItems = ingridients.map(function(item){
+      return <ListItem key={item.id} ingridient={item.text}/>
+    });
+
+    return (<ul>{listItems}</ul>)
+  }
+});
+
+module.exports = List;
